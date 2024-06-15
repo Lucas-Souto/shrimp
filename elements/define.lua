1e8ecbef6f64790b83a10fae78745d3f23ddb1ed
= <contato.lucassouto@outlook.com>
2024-06-15 16:05:53 -0300

    Melhorias.

--- elements/define.lua
+++ elements/define.lua
@@ -1,15 +1,27 @@
 local M = {}
 local current_dir = string.gsub(debug.getinfo(1).source, "^@(.+/)[^/]+$", "%1")
 local main = require("main")
+local js = require("js")
+local defined = false
 
 M.define = function()
+	if defined then
+		return
+	end
+
+	defined = true
+
+	route("/smp/index.js", function (request)
+		return { status = 200, content_type = "text/javascript", body = js.getbody() }
+	end)
+
 	main.define_element
 	{
 		tag = "dataselect",
 		path = current_dir .. "dataselect/index.html",
 		scripts = { current_dir .. "dataselect/index.js" },
 		styles = { current_dir .. "dataselect/index.css" },
-		variables = { "search", "content", "id", "class" }
+		variables = { "search", "content", "id" }
 	}
 end
 
